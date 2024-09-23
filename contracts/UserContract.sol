6b182c28324716c8af9f256d588b7e296a048e09
monickark <monicka.rk@gmail.com>
2024-09-23 11:29:00 +0530

    Contract changes

--- contracts/UserContract.sol
+++ contracts/UserContract.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 contract UserContract {
@@ -15,18 +17,17 @@ constructor(){
     string lastName;
     uint age;
     string city;    
-    uint tipAmount;
-    address payable author;
+    address author;
   }
 event profileTips(string firstName, string lastName, uint age, 
- string city, uint tipAmount, address payable author);
+ string city, address author);
 
 mapping(uint => User) public Users;
 
  function createUser(string memory _firstName, string memory _lastName, uint _age, 
  string memory  _city) public {
     userCount ++;
-     Users[userCount] = User(userCount, _firstName, _lastName, _age, _city, 20, msg.sender);
-    emit profileTips(_firstName, _lastName, _age,  _city, msg.value,  msg.sender);
+     Users[userCount] = User(userCount, _firstName, _lastName, _age, _city, msg.sender);
+    emit profileTips(_firstName, _lastName, _age,  _city,   msg.sender);
  }
 }
